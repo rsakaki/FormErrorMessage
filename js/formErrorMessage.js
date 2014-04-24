06c0912bdd9de81ef4ce0fd2ef33674d66e33c64
rsakaki <rsakaki@ewmjapan.com>
2014-04-24 12:05:25 +0900

    Update formErrorMessage.js

--- js/formErrorMessage.js
+++ js/formErrorMessage.js
@@ -11,7 +11,7 @@ $(function(){
         for(var i = 0; i < dataArray.length; i++){
           $errorTarget = $("[name='"+ dataArray[i].errorInputName + "']");
 
-          // 一つの項目に対して複数入力フィールドがある場合（住所等）
+          // 一つの項目に対して複数入力フィールドがある場合（demo の favorite item 等）
           // 各入力フィールドを .field__input__box で囲う
           if($errorTarget.parents('.field__input__box').length){
             $parent = $errorTarget.parents('.field__input__box');
